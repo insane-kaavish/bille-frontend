590a30ca39eabe69dee6c03fb7cf5bd32e709021
Muhammad Azeem Haider <mh06858@st.habib.edu.pk>
2023-12-19 02:37:58 +0500

    Prediction Screen completed

--- Screens/Prediction.js
+++ Screens/Prediction.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Dimensions, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 import { BarChart } from 'react-native-chart-kit';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from '@react-navigation/native';
-import { MenuProvider, Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
+import {
+  MenuProvider,
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from 'react-native-popup-menu';
 
 const screenWidth = Dimensions.get('window').width;
 
 const App = () => {
-  const navigation = useNavigation(); // Hook to access the navigation prop
+  const navigation = useNavigation();
 
   const chartConfig = {
     backgroundGradientFrom: '#FFF',
@@ -20,75 +33,82 @@ const App = () => {
 
   const data = {
     labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    datasets: [{
-      data: Array.from({ length: 7 }, () => Math.random() * 100),
-    }],
+    datasets: [
+      {
+        data: Array.from({ length: 7 }, () => Math.random() * 100),
+      },
+    ],
   };
 
   return (
     <MenuProvider style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <View style={styles.header}>
-          <TouchableOpacity onPress={() => navigation.goBack()} style={styles.headerIcon}>
-            <Ionicons name="chevron-back" size={30} color="black" />
-          </TouchableOpacity>
-          <Menu>
-            <MenuTrigger style={styles.headerIcon}>
-              <Ionicons name="menu" size={30} color="black" />
-            </MenuTrigger>
-            <MenuOptions style={styles.menuOptionsStyle}>
-              <MenuOption onSelect={() => navigation.navigate('EditProfile')}>
-                <Text style={styles.menuOptionText}>Profile</Text>
-              </MenuOption>
-              <MenuOption onSelect={() => navigation.navigate('Settings')}>
-                <Text style={styles.menuOptionText}>Settings</Text>
-              </MenuOption>
-              <MenuOption onSelect={() => navigation.navigate('HelpCenter')}>
-                <Text style={styles.menuOptionText}>Help center</Text>
-              </MenuOption>
-              <MenuOption onSelect={() => navigation.navigate('SignIn')}>
-                <Text style={styles.menuOptionText}>Sign out</Text>
-              </MenuOption>
-            </MenuOptions>
-          </Menu>
-        </View>
+      <View style={styles.header}>
+        <Menu>
+          <MenuTrigger>
+            <Ionicons name="menu" size={30} color="black" style={styles.menuIcon} />
+          </MenuTrigger>
+          <MenuOptions style={styles.menuOptionsStyle}>
+            <MenuOption onSelect={() => navigation.navigate('EditProfile')}>
+              <Text style={styles.menuOptionText}>Profile</Text>
+            </MenuOption>
+            <MenuOption onSelect={() => navigation.navigate('Settings')}>
+              <Text style={styles.menuOptionText}>Settings</Text>
+            </MenuOption>
+            <MenuOption onSelect={() => navigation.navigate('HelpCenter')}>
+              <Text style={styles.menuOptionText}>Help Center</Text>
+            </MenuOption>
+            <MenuOption onSelect={() => navigation.navigate('SignIn')}>
+              <Text style={styles.menuOptionText}>Sign Out</Text>
+            </MenuOption>
+          </MenuOptions>
+        </Menu>
+      </View>
 
-        <ScrollView style={styles.scrollContainer}>
-          <View style={styles.predictionCard}>
-            <Text style={styles.title}>Predicted Consumption</Text>
-            <View style={styles.consumptionCircle}>
-              <Text style={styles.consumptionValue}>213</Text>
-              <Text style={styles.consumptionUnit}>Predicted Units</Text>
-            </View>
-            <Text style={styles.estimatedBill}>Your estimated bill for this month will be Pkr. 30,000</Text>
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.predictionCard}>
+          <Text style={styles.title}>Predicted Consumption</Text>
+          <View style={styles.consumptionCircle}>
+            <Text style={styles.consumptionValue}>213</Text>
+            <Text style={styles.consumptionUnit}>Predicted Units</Text>
           </View>
-
-          <BarChart
-            style={styles.graphStyle}
-            data={data}
-            width={screenWidth}
-            height={220}
-            yAxisLabel="$"
-            chartConfig={chartConfig}
-            verticalLabelRotation={30}
-          />
-        </ScrollView>
-        
-        <View style={styles.bottomTabBar}>
-          <TouchableOpacity onPress={() => navigation.navigate('DashBoard')}>
-            <Ionicons name="home-outline" size={24} color="#000" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Prediction')}>
-            <Ionicons name="stats-chart-outline" size={24} color="#000" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => console.log('Navigate to Room Wise')}>
-            <Ionicons name="grid-outline" size={24} color="#000" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('EditProfile')}>
-            <Ionicons name="person-outline" size={24} color="#000" />
+          <Text style={styles.estimatedBill}>
+            Your estimated bill for this month will be Pkr. 30,000
+          </Text>
+          <TouchableOpacity
+            style={styles.detailsButton}
+            onPress={() => navigation.navigate('RoomDetails')}
+          >
+            <Text style={styles.detailsButtonText}>View Room Details</Text>
           </TouchableOpacity>
         </View>
-      </SafeAreaView>
+
+        <View style={styles.graphCard}>
+        <BarChart
+          style={styles.graphStyle}
+          data={data}
+          width={screenWidth - 64} // Subtract total horizontal padding and margins
+          height={220}
+          yAxisLabel="$"
+          chartConfig={chartConfig}
+          verticalLabelRotation={30}
+        />
+        </View>
+      </ScrollView>
+
+      <View style={styles.navBar}>
+        <TouchableOpacity onPress={() => navigation.navigate('DashBoard')}>
+          <Ionicons name="home-outline" size={24} color="#000" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Prediction')}>
+          <Ionicons name="stats-chart-outline" size={24} color="#000" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('RoomDetails')}>
+          <Ionicons name="grid-outline" size={24} color="#000" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('EditProfile')}>
+          <Ionicons name="person-outline" size={24} color="#000" />
+        </TouchableOpacity>
+      </View>
     </MenuProvider>
   );
 };
@@ -98,21 +118,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  safeArea: {
-    flex: 1,
-  },
   header: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    justifyContent: 'flex-end',
     padding: 10,
+    zIndex: 1, // Ensures that header is above the other content
   },
-  headerIcon: {
+  menuIcon: {
     padding: 10,
+    zIndex: 1, // Ensures that menu icon is clickable
   },
-  headerTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  menuOptionsStyle: {
+    marginTop: 40,
+    zIndex: 1, // Ensures that menu options are clickable
   },
   scrollContainer: {
     flex: 1,
@@ -130,9 +148,9 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   consumptionCircle: {
-    width: 140,
-    height: 140,
-    borderRadius: 70,
+    width: 160,
+    height: 160,
+    borderRadius: 80,
     borderColor: '#00BCD4',
     borderWidth: 2,
     justifyContent: 'center',
@@ -154,16 +172,46 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 20,
   },
+  detailsButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  detailsButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  graphCard: {
+    backgroundColor: '#f9f9f9',
+    borderRadius: 8,
+    paddingVertical: 16,
+    paddingHorizontal: 16, // Adjust padding as needed
+    marginHorizontal: 16,
+    marginTop: 10,
+    marginBottom: 20,
+    overflow: 'hidden', // Ensures that the graph does not overflow the card
+  },
   graphStyle: {
     marginVertical: 8,
-    borderRadius: 16,
   },
-  bottomTabBar: {
+  navBar: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     borderTopWidth: 1,
     borderTopColor: '#EFEFEF',
     paddingVertical: 10,
+    backgroundColor: '#FFFFFF',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 1, // Ensures that navBar is clickable
+  },
+  menuOptionText: {
+    fontSize: 16,
+    padding: 10,
   },
 });
 
