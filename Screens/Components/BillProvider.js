e4d62bf6973fc7dc113a16c04e5f742f97fd8211
Shahid Mahmood <mm06600@st.habib.edu.pk>
2024-03-21 12:40:01 +0500

    fixed context clearing on logout

--- Screens/Components/BillProvider.js
+++ Screens/Components/BillProvider.js
@@ -149,6 +149,29 @@ export const BillProvider = ({ children }) => {
     setIsMonthlyDataFetched(true);
   };
 
+  const resetData = () => {
+    setUnits(0);
+    setTotalCost(0);
+    setPerUnitCost(0);
+    setLabels([
+      "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+    ]);
+    setActualMonthly([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]);
+    setPredictedMonthly([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]);
+    setIsMonthlyDataFetched(false);
+  };
+
   return (
     <BillContext.Provider
       value={{
@@ -161,6 +184,7 @@ export const BillProvider = ({ children }) => {
         isMonthlyDataFetched,
         fetchPredictedData,
         fetchMonthlyData,
+        resetData,
       }}
     >
       {children}
