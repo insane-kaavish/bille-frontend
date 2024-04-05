9e8ee5717d2ba9df5f586e8711f90e57bd9c2902
Muhammad Azeem Haider <mh06858@st.habib.edu.pk>
2024-04-05 17:17:07 +0500

    Changed the styling of Dashboard screen to incorporate the Tips section.

--- Screens/Dashboard.js
+++ Screens/Dashboard.js
@@ -14,7 +14,7 @@ import { useAuth } from "./Auth/AuthProvider";
 import { useBill } from "./Components/BillProvider";
 import { GlobalStyles } from "./Styles/GlobalStyles";
 
-const height = Dimensions.get("window").height;
+const { height } = Dimensions.get("window");
 
 const DashboardScreen = ({ navigation }) => {
   const { authToken } = useAuth();
@@ -36,66 +36,102 @@ const DashboardScreen = ({ navigation }) => {
     <>
       <Header screenName="Dashboard" navigation={navigation} />
       <View style={GlobalStyles.screenContainer}>
-        <ScrollView style={styles.scrollContainer}>
-          <TouchableOpacity style={styles.MC} onPress={navigateToPrediction}>
-            <View style={styles.MCcircleContainer}>
-              <View style={styles.MCinnermostCircleContainer}>
-                <Image source={require("../extra/assets/OV.png")} />
-              </View>
-            </View>
-            <Text style={styles.MCtitle}>Current Units</Text>
-            <Text style={styles.MCdescription}>
-              Based on your current consumption data, your predicted units are{" "}
-              {units} and consider good.
-            </Text>
-            <Text style={styles.MCunitsCount}>{units}</Text>
-          </TouchableOpacity>
+        <ScrollView style={{flex: 1, padding:"5%"}}>
+          <View style={{ flexDirection: "column"}}>
 
-          <View style={styles.HLcontainer}>
-            <Text style={styles.HLtitle}>Highlights</Text>
-          </View>
+            <View style={[styles.BackgroundContainer, {height: height * 0.2, backgroundColor: "#F1F2FDFF"}]}>
+              <TouchableOpacity onPress={navigateToPrediction}>
 
-          <View style={styles.cardsContainer}>
-            <View style={[styles.card, { backgroundColor: "#7C83ED" }]}>
-              <Text style={styles.cardTitle}>Expected Bill</Text>
-              <Text style={styles.cardAmount}>Rs. {totalCost}</Text>
-              <Text style={styles.cardText}>Based on usage pattern</Text>
-              <View style={styles.cardIconContainer}>
-                <Image source={require("../extra/assets/c11.png")} />
-              </View>
+                <View>
+                  <Text style={ {top:"25%", left:"5%", fontFamily:"Lato-Bold", fontSize:24} }>Current Units</Text>
+                </View>
+
+                <View style={ {left:"70%", bottom:"15%"} }>
+                    <Image source={require("../extra/assets/OV.png")} />
+                    <View>
+                      <Text style={ {left:"3%", bottom:"200%", fontFamily:"Lato-Bold", fontSize:24, color:"white"} }> {units} </Text>
+                    </View>
+                </View>
+
+                <View>
+                  <Text style={ {width:"70%" ,bottom:"90%", left:"5%", fontFamily:"Lato-Regular", fontSize:18, justifyContent:"center"} }>
+                    Based on your current consumption data, your predicted units are{" "}
+                    {units} and consider good.
+                  </Text>
+                </View>
+
+              </TouchableOpacity>
             </View>
 
-            <View style={[styles.card, { backgroundColor: "#2ACCCF" }]}>
-              <Text style={styles.cardTitle}>Per Unit Price</Text>
-              <Text style={styles.cardAmount}>Rs. {perUnitCost}</Text>
-              <Text style={styles.cardText}>Based on the slab rates</Text>
-              <View style={styles.cardIconContainer}>
-                <Image source={require("../extra/assets/c12.png")} />
-              </View>
-            </View>
-          </View>
+            <View style={[styles.BackgroundContainer, {height: height * 0.285, backgroundColor: "white"}]}>
 
-          <View style={styles.MRContainer}>
-            <Text style={styles.MRtext}>Monthly Report</Text>
+              <View style= {{}}>
+                <Text style={ styles.HeadingContainer }>Highlights</Text>
+              </View>
+
+              <View style={ {flexDirection:"row", justifyContent: "space-between"} }>
+                <View style={[styles.CardContainer, { backgroundColor: "#7C83ED" }]}>
+                  <Text style={ [styles.HighlightsCardText, {fontSize: 18} ] }>Expected Bill:</Text>
+                  <Text style={ [styles.HighlightsCardText, {fontSize: 28, lineHeight: 40} ] }>Rs. {totalCost}</Text>
+                  <Text style={ [styles.HighlightsCardText, {fontSize: 16} ] }>Based on usage pattern</Text>
+
+                  <View style={ {left:"60%"} }>
+                    <Image source={require("../extra/assets/c11.png")} />
+                  </View>
+
+                </View>
+
+                <View style={[styles.CardContainer, {backgroundColor: "#2ACCCF" }]}>
+                  <Text style={ [styles.HighlightsCardText, {fontSize: 18} ] }>Per Unit Price:</Text>
+                  <Text style={ [styles.HighlightsCardText, {fontSize: 28, lineHeight: 40} ] }>Rs. {perUnitCost}</Text>
+                  <Text style={ [styles.HighlightsCardText, {fontSize: 16} ] }>Based on the slab rates </Text>
+
+                  <View style={ {left:"60%"} }>
+                    <Image source={require("../extra/assets/c12.png")} />
+                  </View>
+
+                </View>
+
+              </View>
+
+            </View>
+
+            <View style={[styles.BackgroundContainer, {height: height * 0.16, backgroundColor: "white",}]}>
+              <View style= { {} }>
+                <Text style={ styles.HeadingContainer }>Monthly Report</Text>
+              </View>
+
+              <View style={ {flexDirection:"row", justifyContent: "space-between"} }>
+                <TouchableOpacity style={[styles.CardContainer, styles.MonthlyReportButton]} onPress={navigateToRoomOverview}>
+
+                  <View style={{ flexDirection: "row", alignItems:"center"}}> 
+                    <Text style={ {width:"70%", fontFamily: "Lato-Bold", fontSize: 24} }>Room Report</Text>
+
+                    <View style={ {} }>
+                      <Image source={require("../extra/assets/RRIcon.png")} />
+                    </View>
+                  </View>
+
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.CardContainer, styles.MonthlyReportButton]} onPress={navigateToPrediction}>
+                  
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Text style={{fontFamily: "Lato-Bold", fontSize: 24 }}>Monthly{"\n"}Units</Text>
+                    
+                    <View style={{}}>
+                      <Image source={require("../extra/assets/MUGIcon.png")} />
+                    </View>
+                  </View>
+
+                </TouchableOpacity>
+
+
+              </View>
+
+            </View>
+
           </View>
-            <TouchableOpacity
-              style={styles.RRcontainer}
-              onPress={navigateToRoomOverview}
-            >
-              <Text style={styles.RRText}>Room Report</Text>
-              <View style={styles.RRicon}>
-                <Image source={require("../extra/assets/RRIcon.png")} />
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.MUGcontainer}
-              onPress={navigateToPrediction}
-            >
-              <Text style={styles.MUGText}> Monthly{"\n"}   Units</Text>
-              <View style={styles.MUGicon}>
-                <Image source={require("../extra/assets/MUGIcon.png")} />
-              </View>
-            </TouchableOpacity>
         </ScrollView>
         <Navbar />
       </View>
@@ -104,179 +140,34 @@ const DashboardScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flex: 1,
-  },
-  MC: {
-    width: "90%",
-    height: "20%",
-    alignSelf: "center",
-    justifyContent: "center",
-    marginTop: "6%",
-    backgroundColor: "#F1F2FDFF",
+  BackgroundContainer: {
+    width: "100%",
     borderRadius: 16,
-    elevation: 6, // for the main shadow
-    shadowColor: "#000", // color of the shadow
-    shadowOffset: { width: 0, height: 0 }, // same as the CSS code
-    shadowOpacity: 0.3, // opacity of the shadow
-    shadowRadius: 1, // blur radius of the shadow
+    position: "relative"
   },
-  MCcircleContainer: {
-    left: "74%",
-    top: "13%",
-    position: "relative",
-  },
-  MCtitle: {
-    left: "5%",
-    top: "-30%",
-    position: "relative",
-    color: "#171A1F",
-    fontSize: 18,
-    fontFamily: "Lato-Bold",
-    fontWeight: "600",
-    lineHeight: 28,
-  },
-  MCdescription: {
-    width: "50%",
-    left: "5%",
-    top: "-25%",
-    position: "relative",
-    color: "#171A1F",
-    fontSize: 14,
-    fontFamily: "Lato-Bold",
-    fontWeight: "400",
-    lineHeight: 22,
-  },
-  MCunitsCount: {
-    left: "78.5%",
-    top: "7.5%",
-    position: "absolute",
-    color: "white",
-    fontSize: 24,
-    fontFamily: "Lato-Bold",
-    fontWeight: "700",
-    lineHeight: 36,
-    alignContent: "center",
-    textAlign: "center",
-    transform: [{ translateX: -1.75 }],
-  },
-  HLcontainer: {
-    left: "5%",
-    top: "3%",
-  },
-  HLtitle: {
-    color: "#171A1F",
-    fontSize: 20,
-    fontFamily: "Lato-Bold",
-    fontWeight: "600",
-    lineHeight: 30,
-  },
-  cardsContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: "7.5%",
-    paddingHorizontal: "4%",
-  },
-  card: {
+  CardContainer: {
     width: "48%",
-    height: "175%",
-    backgroundColor: "#F1F2FD",
     borderRadius: 16,
     padding: "3%",
   },
-  cardTitle: {
+  HeadingContainer: {
+    top:"25%", 
+    fontFamily:"Lato-Bold", 
+    fontSize:24, 
+    paddingBottom: "5%"
+  },
+  HighlightsCardText: {
     color: "white",
-    fontSize: 18,
     fontFamily: "Lato-Bold",
-    fontWeight: "500",
   },
-  cardAmount: {
-    color: "white",
-    fontSize: 28,
-    fontFamily: "Lato-Bold",
-    fontWeight: "500",
-    lineHeight: 36,
-  },
-  cardText: {
-    color: "white",
-    fontSize: 12,
-    fontFamily: "Lato-Bold",
-    fontWeight: "400",
-  },
-  cardIconContainer: {
-    width: "40%",
-    height: "40%",
-    position: "absolute",
-    right: 10,
-    bottom: 10,
-  },
-  MRContainer: {
-    marginLeft: "3.5%", // Aligning "Monthly Report" with "Highlights"
-    marginTop: "20%", // Adjust the margin as needed
-    padding: "1.5%", // Adjust the padding as needed
-  },
-  MRtext: {
-    color: "#171A1F",
-    fontSize: 20,
-    fontFamily: "Lato-Bold",
-    fontWeight: "600",
-    lineHeight: 30,
-  },
-  RRcontainer: {
-    width: "44%",
-    height: "15%",
-    left: "3.5%",
-    backgroundColor: "#F1F2FDFF",
-    borderRadius: 16,
-    elevation: 6, // for the main shadow
-    shadowColor: "#000", // color of the shadow
-    shadowOffset: { width: 0, height: 0 }, // same as the CSS code
-    shadowOpacity: 0.3, // opacity of the shadow
-    shadowRadius: 1, // blur radius of the shadow
-  },
-  RRText: {
-    fontFamily: "Lato-Regular",
-    top: "5%",
-    fontSize: 22,
-    fontWeight: "700",
-    lineHeight: 30,
-    color: "#323842",
-    flexWrap: "wrap",
-    marginLeft: "40%", // Adjust the left margin to center the text
-    marginTop: "10%", // Adjust the margin as needed
-  },
-  RRicon: {
-    left: "6%", // Adjust the left position as needed
-    top: "-48%",  // Adjust the top position as needed
-  },
-  MUGcontainer: {
-    width: "44%",
-    height: "15%",
-    left: "52.25%",
-    top: "-15%",
-    backgroundColor: "#F1F2FDFF", // white
-    borderRadius: 16,
-    elevation: 6, // for the main shadow
-    shadowColor: "#000", // color of the shadow
-    shadowOffset: { width: 0, height: 0 }, // same as the CSS code
-    shadowOpacity: 0.3, // opacity of the shadow
-    shadowRadius: 1, // blur radius of the shadow
-  },
-  MUGText: {
-    fontFamily: "Lato-Regular",
-    top: "5%", // Adjust the top position as needed
-    fontSize: 22,
-    fontWeight: "700",
-    lineHeight: 30,
-    color: "#323842",
-    flexWrap: "wrap",
-    marginLeft: "40%", // Adjust the left margin to center the text
-    marginTop: "10%", // Adjust the margin as needed
-  },
-  MUGicon: {
-    left: "8%", // Adjust the left position as needed
-    top: "-50%", // Adjust the top position as needed
-  },
+  MonthlyReportButton: {
+    backgroundColor: "#F1F2FDFF", 
+    elevation: 2, 
+    shadowColor: "#000", 
+    shadowOpacity: 0.2, 
+    shadowRadius: 1
+  }
+  
 });
 
 export default DashboardScreen;
