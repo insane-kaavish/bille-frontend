90b460af99a4c2315bf15fa974428b92c59689a3
Syed Ibrahim Ali <sh06565@st.habib.edu.pk>
2024-04-08 00:39:37 +0500

    initial-tips

--- Screens/Dashboard.js
+++ Screens/Dashboard.js
@@ -7,6 +7,8 @@ import {
   Text,
   TouchableOpacity,
   Dimensions,
+  Modal,
+  TouchableWithoutFeedback,
 } from "react-native";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
@@ -20,6 +22,14 @@ const DashboardScreen = ({ navigation }) => {
   const { authToken } = useAuth();
   const { units, totalCost, perUnitCost, fetchPredictedData } = useBill();
 
+  const [modalVisible, setModalVisible] = useState(false); // Added modalVisible state
+  const [modalContent, setModalContent] = useState({
+    title: "",
+    subtitle: "",
+    details: "",
+  });
+
+
   useEffect(() => {
     console.log("Fetching predicted data")
     fetchPredictedData();
@@ -31,6 +41,74 @@ const DashboardScreen = ({ navigation }) => {
   const navigateToRoomOverview = () => {
     navigation.navigate("RoomOverview");
   };
+
+  const handleRecommendationPress = (title) => {
+    switch (title) {
+      case "Inverter Air Conditioner":
+        setModalContent({
+          title: "Inverter Air Conditioner",
+          subtitle: "Did you know?",
+          details:
+            "Inverter air conditioners can save up to 50% of your annual electricity consumption compared to conventional AC units.\n\n" +
+            "Energy-saving tip:\nTo save energy, set your AC thermostat to 26°C and regularly clean and replace its filters.",
+        });
+        break;
+      case "LED Lighting":
+        setModalContent({
+          title: "LED Lighting",
+          subtitle: "Did you know?",
+          details:
+            "LED lights can save up to 75% of your annual energy consumption and last up to 25 times longer than incandescent lights.\n\n" +
+            "Energy-saving tip:\nTo conserve energy more efficiently, install smart home technology and draw your curtains during the day to utilize sunlight.",
+        });
+        break;
+      case "Heat Rejection and Insulation Sheets":
+        setModalContent({
+          title: "Heat Rejection and Insulation Sheets",
+          subtitle: "Did you know?",
+          details:
+            "Heat rejection and insulation sheets can reduce up to 78% of the sun's heat from entering your home, thereby reducing cooling costs.\n\n" +
+            "Energy-saving tip:\nKeep your home cool and create a comfortable space for your family by installing heat rejection and insulation sheets.",
+        });
+        break;
+      case "Energy-Intensive Appliances":
+        setModalContent({
+          title: "Energy-Intensive Appliances",
+          subtitle: "Did you know?",
+          details:
+            "Old energy-intensive appliances cost an average homeowner up to 45% on their annual consumption.\n\n" +
+            "Energy-saving tip:\nWhen buying appliances, look for the Energy Star label that identifies products in terms of energy efficiency. Make the smart switch to energy-efficient appliances. If you are using energy-intensive appliances, use them during off-peak hours (10 pm to 6 am) to reduce your consumption load.",
+        });
+        break;
+      case "Energy Efficient UPS":
+        setModalContent({
+          title: "Energy Efficient UPS",
+          subtitle: "Did you know?",
+          details:
+            "Energy-efficient UPS can reduce your annual energy loss by up to 55%.\n\n" +
+            "Energy-saving tip:\nWhen buying a UPS, look for the Energy Star label that identifies products by their efficiency. Servicing equipment regularly ensures optimal efficiency.",
+        });
+        break;
+      case "Phantom Energy Consumption":
+        setModalContent({
+          title: "Phantom Energy Consumption",
+          subtitle: "Did you know?",
+          details:
+            "Phantom energy, which is unconsumed or wasted energy, contributes up to 10% of your annual electricity bill.\n\n" +
+            "Energy-saving tip:\nIt is best to unplug electronic devices and kitchen appliances when not in use to save energy.",
+        });
+        break;
+      default:
+        setModalContent({
+          title: "",
+          subtitle: "",
+          details: "",
+        });
+        break;
+    }
+    setModalVisible(true);
+  };
+  
 
   return (
     <>
@@ -132,6 +210,92 @@ const DashboardScreen = ({ navigation }) => {
             </View>
 
           </View>
+
+          {/* Electricity Conservation tips */}
+          <View style={styles.Lastcontainer}>
+            <Text style={[styles.HLtitle, { fontWeight: 'bold' }]}>Conservation Tips</Text>
+          </View>
+          <ScrollView horizontal={true} style={styles.graphCard}>
+            {/* Container 1 */}
+            <TouchableOpacity onPress={() => handleRecommendationPress("Inverter Air Conditioner")} style={[styles.recommendationContainer, { backgroundColor: "#F1F2FD" }]}>
+              <Text style={[styles.containerTitle, { fontSize: 18, marginTop: 10 }]}>Inverter Air Conditioner</Text>
+              <Text style={[styles.containerSubTitle, { fontSize: 16 }]}>Did you know?</Text>
+              <Text numberOfLines={2} ellipsizeMode="tail" style={[styles.containerText, { fontSize: 16, marginTop: 5 }]}>
+                Inverter air conditioners can save up to 50% of your annual electricity consumption compared to conventional AC units.
+              </Text>
+            </TouchableOpacity>
+            
+            {/* Container 2 */}
+            <TouchableOpacity onPress={() => handleRecommendationPress("LED Lighting")} style={[styles.recommendationContainer, { backgroundColor: "#FFE3E3" }]}>
+              <Text style={[styles.containerTitle, { fontSize: 18, marginTop: 10 }]}>LED Lighting</Text>
+              <Text style={[styles.containerSubTitle, { fontSize: 16 }]}>Did you know?</Text>
+              <Text numberOfLines={2} ellipsizeMode="tail" style={[styles.containerText, { fontSize: 16, marginTop: 5 }]}>
+                LED lights can save up to 75% of your annual energy consumption and last up to 25 times longer than incandescent lights.
+              </Text>
+            </TouchableOpacity>
+
+            {/* Container 3 */}
+            <TouchableOpacity onPress={() => handleRecommendationPress("Heat Rejection and Insulation Sheets")} style={[styles.recommendationContainer, { backgroundColor: "#C4F2E6" }]}>
+              <Text style={[styles.containerTitle, { fontSize: 18, marginTop: 10 }]}>Heat Rejection and Insulation Sheets</Text>
+              <Text style={[styles.containerSubTitle, { fontSize: 16 }]}>Did you know?</Text>
+              <Text numberOfLines={2} ellipsizeMode="tail" style={[styles.containerText, { fontSize: 16, marginTop: 5 }]}>
+                Heat rejection and insulation sheets can reduce up to 78% of the sun's heat from entering your home, thereby reducing cooling costs.
+              </Text>
+            </TouchableOpacity>
+
+            {/* Container 4 */}
+            <TouchableOpacity onPress={() => handleRecommendationPress("Energy-Intensive Appliances")} style={[styles.recommendationContainer, { backgroundColor: "#FFD6A5" }]}>
+              <Text style={[styles.containerTitle, { fontSize: 18, marginTop: 10 }]}>Energy-Intensive Appliances</Text>
+              <Text style={[styles.containerSubTitle, { fontSize: 16 }]}>Did you know?</Text>
+              <Text numberOfLines={2} ellipsizeMode="tail" style={[styles.containerText, { fontSize: 16, marginTop: 5 }]}>
+                Old energy-intensive appliances cost an average homeowner up to 45% on their annual consumption.
+              </Text>
+            </TouchableOpacity>
+
+            {/* Container 5 */}
+            <TouchableOpacity onPress={() => handleRecommendationPress("Energy Efficient UPS")} style={[styles.recommendationContainer, { backgroundColor: "#FFDDDD" }]}>
+              <Text style={[styles.containerTitle, { fontSize: 18, marginTop: 10 }]}>Energy Efficient UPS</Text>
+              <Text style={[styles.containerSubTitle, { fontSize: 16 }]}>Did you know?</Text>
+              <Text numberOfLines={2} ellipsizeMode="tail" style={[styles.containerText, { fontSize: 16, marginTop: 5 }]}>
+                Energy-efficient UPS can reduce your annual energy loss by up to 55%.
+              </Text>
+            </TouchableOpacity>
+
+            {/* Container 6 */}
+            <TouchableOpacity onPress={() => handleRecommendationPress("Phantom Energy Consumption")} style={[styles.recommendationContainer, { backgroundColor: "#C2E5FC" }]}>
+              <Text style={[styles.containerTitle, { fontSize: 18, marginTop: 10 }]}>Phantom Energy Consumption</Text>
+              <Text style={[styles.containerSubTitle, { fontSize: 16 }]}>Did you know?</Text>
+              <Text numberOfLines={2} ellipsizeMode="tail" style={[styles.containerText, { fontSize: 16, marginTop: 5 }]}>
+                Phantom energy, which is unconsumed or wasted energy, contributes up to 10% of your annual electricity bill.
+              </Text>
+            </TouchableOpacity>
+            
+            {/* Add more containers as needed */}
+          </ScrollView>
+
+{/* Modal for Expanded Recommendation */}
+<Modal
+  animationType="slide"
+  transparent={true}
+  visible={modalVisible}
+  onRequestClose={() => setModalVisible(false)}
+>
+  <View style={styles.centeredView}>
+    <View style={styles.modalView}>
+      <Text style={styles.modalTitle}>{modalContent.title}</Text>
+      <Text style={styles.modalText}>{modalContent.subtitle}</Text>
+      <Text style={styles.modalText}>{modalContent.details}</Text>
+      <TouchableOpacity
+        onPress={() => setModalVisible(false)}
+        style={styles.closeButton}
+      >
+        <Text style={styles.closeButtonText}>Close</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+</Modal>
+
+
         </ScrollView>
         <Navbar />
       </View>
@@ -144,6 +308,81 @@ const styles = StyleSheet.create({
     width: "100%",
     borderRadius: 16,
     position: "relative"
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  Lastcontainer: {
+    marginBottom: 20,
+  },
+  HLtitle: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  graphCard: {
+    flexDirection: 'row',
+  },
+  recommendationContainer: {
+    width: 300,
+    marginRight: 20,
+    padding: 15,
+    borderRadius: 10,
+  },
+  containerTitle: {
+    fontWeight: 'bold',
+  },
+  containerSubTitle: {
+    marginTop: 5,
+  },
+  containerText: {
+    marginTop: 5,
+  },
+    centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalTitle: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  modalText: {
+    marginBottom: 15,
+    fontSize: 18,
+    textAlign: "center",
+  },
+  closeButton: {
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#2196F3",
+  },
+  closeButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
   CardContainer: {
     width: "48%",
