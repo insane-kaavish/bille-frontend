1ac7f8f4cecbc4c0348ea132271a6d1be4cb1877
Syed Ibrahim Ali <sh06565@st.habib.edu.pk>
2024-04-08 01:43:56 +0500

    contactus-UIimproved

--- Screens/Profile/Contact.js
+++ Screens/Profile/Contact.js
@@ -5,13 +5,10 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  KeyboardAvoidingView,
-  ScrollView,
 } from "react-native";
-
+import { AntDesign } from "@expo/vector-icons";
 import Navbar from "../Components/Navbar";
 import Header from "../Components/Header";
-
 import { GlobalStyles } from "../Styles/GlobalStyles";
 
 const ContactScreen = ({ navigation }) => {
@@ -20,14 +17,9 @@ const ContactScreen = ({ navigation }) => {
   const [message, setMessage] = useState("");
 
   const handleSubmit = () => {
-    // Implement logic to handle the contact form submission, e.g., make an API call
-
-    // For now, we'll just log the values
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Message:", message);
-
-    // You can add further logic, like clearing the form or displaying a success message
     setName("");
     setEmail("");
     setMessage("");
@@ -36,25 +28,22 @@ const ContactScreen = ({ navigation }) => {
   return (
     <>
       <Header screenName="Contact Us" navigation={navigation} />
-
-      <KeyboardAvoidingView
-        style={{ flex: 1, backgroundColor: "#F8F8F8" }}
-        behavior="padding"
-      >
-        <ScrollView
-          contentContainerStyle={GlobalStyles.screenContainer}
-          showsVerticalScrollIndicator={false}
-        >
-          <View style={styles.formContainer}>
-            <Text style={styles.label}>Your Name:</Text>
+      <View style={GlobalStyles.screenContainer}>
+        <View style={styles.formContainer}>
+          <Text style={styles.label}>Your Name:</Text>
+          <View style={styles.inputContainer}>
+            <AntDesign name="user" size={24} color="black" />
             <TextInput
               style={styles.input}
               value={name}
               onChangeText={(text) => setName(text)}
               placeholder="Enter your name"
             />
+          </View>
 
-            <Text style={styles.label}>Your Email:</Text>
+          <Text style={styles.label}>Your Email:</Text>
+          <View style={styles.inputContainer}>
+            <AntDesign name="mail" size={24} color="black" />
             <TextInput
               style={styles.input}
               value={email}
@@ -62,24 +51,25 @@ const ContactScreen = ({ navigation }) => {
               placeholder="Enter your email"
               keyboardType="email-address"
             />
+          </View>
 
-            <Text style={styles.label}>Your Message:</Text>
+          <Text style={styles.label}>Your Message:</Text>
+          <View style={[styles.inputContainer, { height: 100 }]}>
+            <AntDesign name="message1" size={24} color="black" />
             <TextInput
-              style={[styles.input, styles.messageInput]}
+              style={[styles.input, { height: 80 }]}
               value={message}
               onChangeText={(text) => setMessage(text)}
               placeholder="Enter your message"
               multiline
-              numberOfLines={4}
             />
           </View>
+        </View>
 
-          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-            <Text style={styles.buttonText}>Submit</Text>
-          </TouchableOpacity>
-        </ScrollView>
-      </KeyboardAvoidingView>
-
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
       <Navbar />
     </>
   );
@@ -87,35 +77,39 @@ const ContactScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   formContainer: {
-    marginHorizontal: 20,
-    marginTop: 20,
+    marginBottom: 20,
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 10,
   },
   label: {
     fontSize: 16,
     marginBottom: 5,
-    color: "#333333",
+    color: "black",
   },
   input: {
-    height: 40,
-    borderColor: "#CCCCCC",
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-    backgroundColor: "#FFFFFF",
-  },
-  messageInput: {
-    height: 100,
-    paddingTop: 10,
+    flex: 1,
+    marginLeft: 10,
+    paddingVertical: 8,
+    fontSize: 16,
+    color: "black",
   },
   button: {
-    backgroundColor: "#535CE8",
+    backgroundColor: "#535CE8", // Blue color
     borderRadius: 20,
     paddingVertical: 12,
     alignItems: "center",
-    width: "70%",
+    width: "50%",
     alignSelf: "center",
     marginVertical: 20,
+    elevation: 3,
   },
   buttonText: {
     color: "white",
