3df80739813fafdbfb43d2bed4e26df7571fed13
Syed Ibrahim Ali <sh06565@st.habib.edu.pk>
2024-04-22 01:51:53 +0500

    splashscreen&signup

--- Screens/Auth/Signup.js
+++ Screens/Auth/Signup.js
@@ -215,9 +215,9 @@ const SignupScreen = ({ navigation }) => {
 
   return (
     <>
-      <ScrollView style={[GlobalStyles.screenContainer, styles.container]}
-      contentContainerStyle={styles.scrollViewContent}
-      >
+      <ScrollView style={[GlobalStyles.screenContainer, styles.container]}contentContainerStyle={styles.scrollViewContent}>
+      <Text style={styles.header}>Welcome! Create Your Account👋</Text>
+
         <View
           style={[
             styles.inputContainer,
@@ -327,7 +327,7 @@ const SignupScreen = ({ navigation }) => {
         <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
           <Text style={styles.footerText}>
             Already have an account?{" "}
-            <Text style={styles.footerTextHighlight}>Login</Text>
+            <Text style={styles.footerTextHighlight}>Sign In</Text>
           </Text>
         </TouchableOpacity>
       </ScrollView>
@@ -340,6 +340,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.white,
     padding: 10,
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: "700",
+    marginBottom: 20,
   },
   inputContainer: {
     width: "90%",
