b629c628234af38294bec2b87f1b702cf59cab6a
Syed Ibrahim Ali <sh06565@st.habib.edu.pk>
2024-04-22 00:56:12 +0500

    Navbar-V1

--- Screens/Components/NavBar.js
+++ Screens/Components/NavBar.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { TouchableOpacity, StyleSheet, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"; // Import modern icons
 import { MaterialIcons } from "@expo/vector-icons";
 
 const Navbar = () => {
-  const navigation = useNavigation(); // Hook to access the navigation prop
+  const navigation = useNavigation();
 
   const navigateToOverview = () => {
     navigation.navigate("Dashboard");
@@ -22,13 +21,13 @@ const Navbar = () => {
   return (
     <View style={styles.navBar}>
       <TouchableOpacity onPress={navigateToOverview} style={styles.navtab}>
-        <MaterialCommunityIcons name="home-outline" size={32} color="#555" />
+        <MaterialIcons name="home" size={28} color="#666" />
       </TouchableOpacity>
       <TouchableOpacity onPress={navigateToPrediction} style={styles.navtab}>
-        <MaterialCommunityIcons name="chart-bar" size={32} color="#555" />
+        <MaterialIcons name="bar-chart" size={28} color="#666" />
       </TouchableOpacity>
       <TouchableOpacity onPress={navigateToRoomWise} style={styles.navtab}>
-        <MaterialIcons name="hotel" size={32} color="#555" />
+        <MaterialIcons name="meeting-room" size={28} color="#666" />
       </TouchableOpacity>
     </View>
   );
@@ -38,8 +37,8 @@ const styles = StyleSheet.create({
   navBar: {
     flexDirection: "row",
     justifyContent: "space-around",
-    borderTopWidth: 1,
-    borderTopColor: "#EFEFEF",
+    borderTopWidth: 0.2,
+    borderTopColor: "#E0E0E0",
     paddingVertical: 10,
     backgroundColor: "#FFFFFF",
     position: "absolute",
